100fc116b32f4212e878ea3bb77eb1f3fd039d39
Rhittum <rhittum1802@gmail.com>
2025-08-30 00:17:53 +0000

    added the visitor interface, and the abstract syntax tree test

--- src/main/java/com/craftinginterpreters/tool/GenerateAST.java
+++ src/main/java/com/craftinginterpreters/tool/GenerateAST.java
@@ -13,10 +13,10 @@ public class GenerateAST {
 		}
 		String outputDir = args[0];
 		defineAst(outputDir, "Expr", Arrays.asList(
-					"Binary: Expr left, Token operator, Expr right",
+					"Binary   : Expr left, Token operator, Expr right",
 					"Grouping : Expr expression",
-					"Literal : Object value",
-					"Unary : Toke operator, Expr right"));
+					"Literal  : Object value",
+					"Unary    : Token operator, Expr right"));
 	}
 
 	private static void defineAst(String outputDir, String baseName, List<String> types) throws IOException {
@@ -29,14 +29,30 @@ public class GenerateAST {
 		writer.println();
 		writer.println("abstract class " + baseName + " {");
 
+		defineVisitor(writer, baseName, types);
+
 		for (String type: types) {
 			String className = type.split(":")[0].trim();
 			String fields = type.split(":")[1].trim();
 			defineType(writer, baseName, className, fields);
 		}
 
+		writer.println();
+		writer.println("  abstract <R> R accept(Visitor<R> visitor);");
+
 		writer.println("}");
 		writer.close();
+	}
+
+	private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+		writer.println("  interface Visitor<R> {");
+
+		for(String type: types) {
+			String typeName = type.split(":")[0].trim();
+			writer.println("    R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
+		}
+
+		writer.println("  }");
 	}
 
 	private static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
@@ -53,8 +69,14 @@ public class GenerateAST {
 		writer.println("    }");
 
 		writer.println();
+		writer.println("    @Override");
+		writer.println("    <R> R accept(Visitor<R> visitor) {");
+		writer.println("      return visitor.visit" + className + baseName + "(this);");
+		writer.println("    }");
+
+		writer.println();
 		for (String field: fields) {
-			writer.print("    final " + field + ";");
+			writer.println("    final " + field + ";");
 		}
 
 		writer.println("  }");
